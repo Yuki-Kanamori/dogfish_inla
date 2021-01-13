7258913e11bbac87569ab27b3a5593f4c4e18875
Yuki-Kanamori <y.kanamory@gmail.com>
2021-01-13 11:27:11 +0900

    added a map

--- make_data.R
+++ make_data.R
@@ -1,4 +1,7 @@
 require(tidyverse)
+require(ggplot2)
+require(maps)
+
 
 dir1 = "/Users/Yuki/Dropbox/same"
 
@@ -13,13 +16,13 @@ for(i in 1:length(files)){
   data = read.csv(paste0(files[i]), fileEncoding = "CP932")
   
   if("Same" %in% colnames(data)){
-    data2 = data %>% select(CatchDate, Effort_tow, Same) %>% mutate(sp = "Same") %>% dplyr::rename(kg = Same)
+    data2 = data %>% select(CatchDate, Effort_tow, Same, Latitude, Longitude) %>% mutate(sp = "Same") %>% dplyr::rename(kg = Same)
     kg = data2$kg
     kg[is.na(kg)] = 0
     data2$kg = kg
   }
   if("SameEi" %in% colnames(data)){
-    data2 = data %>% select(CatchDate, Effort_tow, SameEi) %>% mutate(sp = "SameEi") %>% dplyr::rename(kg = SameEi)
+    data2 = data %>% select(CatchDate, Effort_tow, SameEi, Latitude, Longitude) %>% mutate(sp = "SameEi") %>% dplyr::rename(kg = SameEi)
     kg = data2$kg
     kg[is.na(kg)] = 0
     data2$kg = kg
@@ -32,8 +35,13 @@ js2 = js %>% mutate(year = as.numeric(str_sub(CatchDate, 1, 4)), month = as.nume
 summary(js2)
 unique(js2$year)
 
-setwd(dir1)
-write.csv(js2, "js.csv", fileEncoding = "CP932")
+# 緯度経度が間違っている
+lat = js2 %>% filter(Latitude > 45)
+summary(lat)
+
+
+# setwd(dir1)
+# write.csv(js2, "js.csv", fileEncoding = "CP932")
 
 
 
@@ -48,13 +56,13 @@ for(i in 1:length(files)){
   data = read.csv(paste0(files[i]), fileEncoding = "CP932")
   
   if("Same" %in% colnames(data)){
-    data2 = data %>% select(CatchDate, Effort_tow, Same) %>% mutate(sp = "Same") %>% dplyr::rename(kg = Same)
+    data2 = data %>% select(CatchDate, Effort_tow, Same, Latitude, Longitude) %>% mutate(sp = "Same") %>% dplyr::rename(kg = Same)
     kg = data2$kg
     kg[is.na(kg)] = 0
     data2$kg = kg
   }
   if("Aburatsunozame" %in% colnames(data)){
-    data2 = data %>% select(CatchDate, Effort_tow, Aburatsunozame) %>% mutate(sp = "Aburatsunozame") %>% dplyr::rename(kg = Aburatsunozame)
+    data2 = data %>% select(CatchDate, Effort_tow, Aburatsunozame, Latitude, Longitude) %>% mutate(sp = "Aburatsunozame") %>% dplyr::rename(kg = Aburatsunozame)
     kg = data2$kg
     kg[is.na(kg)] = 0
     data2$kg = kg
@@ -68,5 +76,33 @@ po2 = po %>% mutate(year = as.numeric(str_sub(CatchDate, 1, 4)), month = as.nume
 summary(po2)
 unique(po2$year)
 
-setwd(dir1)
-write.csv(po2, "po.csv", fileEncoding = "CP932")
+# setwd(dir1)
+# write.csv(po2, "po.csv", fileEncoding = "CP932")
+
+
+# data area -----------------------------------------------------
+summary(js2)
+summary(po2)
+
+same = rbind(po2, js2) %>% dplyr::rename(lon = Longitude, lat = Latitude)
+summary(same)
+same = same %>% filter(lat < 42) #うまいこと北海道のデータは除去できる
+
+map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
+world_map = map_data("world")
+region2 = subset(world_map, world_map$region == region)
+local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(same$lon)-0.2, max(same$lon)+0.2), ylim = c(min(same$lat)-0.2, max(same$lat)+0.2))
+th = theme(panel.grid.major = element_blank(),
+           panel.grid.minor = element_blank(),
+           axis.text.x = element_text(size = rel(1.5)),
+           axis.text.y = element_text(size = rel(1.5)),
+           axis.title.x = element_text(size = rel(1.5)),
+           axis.title.y = element_text(size = rel(1.5)),
+           legend.title = element_text(size = 13))
+p = geom_point(data = same, aes(x = lon, y = lat, colour = kg), shape = 16, size = 1)
+# f = facet_wrap( ~ Category, ncol = ncol)
+c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
+labs = labs(x = "Longitude", y = "Latitude", colour = "kg")
+
+fig = local_map+theme_bw()+th+p+c+labs
+ggsave(filename = "map_same.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
