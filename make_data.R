7b495947cac64cec83702c21b6187661266bb3ad
Yuki-Kanamori <y.kanamory@gmail.com>
2021-01-26 10:08:58 +0900

    revised the code for JS and not deleted in Hokkaido area

--- make_data.R
+++ make_data.R
@@ -5,8 +5,47 @@ require(maps)
 
 dir1 = "/Users/Yuki/Dropbox/same"
 
-# JS ------------------------------------------------------------
-dir_js = "/Users/Yuki/Dropbox/same/JS"
+# # JS ------------------------------------------------------------
+# dir_js = "/Users/Yuki/Dropbox/same/JS"
+# setwd(dir_js)
+# path = dir_js
+# files = list.files(path)
+# 
+# js = NULL
+# for(i in 1:length(files)){
+#   data = read.csv(paste0(files[i]), fileEncoding = "CP932")
+#   
+#   if("Same" %in% colnames(data)){
+#     data2 = data %>% select(CatchDate, Effort_tow, Same, Latitude, Longitude) %>% mutate(sp = "Same") %>% dplyr::rename(kg = Same)
+#     kg = data2$kg
+#     kg[is.na(kg)] = 0
+#     data2$kg = kg
+#   }
+#   if("SameEi" %in% colnames(data)){
+#     data2 = data %>% select(CatchDate, Effort_tow, SameEi, Latitude, Longitude) %>% mutate(sp = "SameEi") %>% dplyr::rename(kg = SameEi)
+#     kg = data2$kg
+#     kg[is.na(kg)] = 0
+#     data2$kg = kg
+#   }
+#   js = rbind(js, data2)
+# }
+# unique(js$CatchDate)
+# summary(js$CatchDate)
+# js2 = js %>% mutate(year = as.numeric(str_sub(CatchDate, 1, 4)), month = as.numeric(str_sub(CatchDate, 5, 6)), day = as.numeric(str_sub(CatchDate, 7, 8)))
+# summary(js2)
+# unique(js2$year)
+# 
+# # 緯度経度が間違っている
+# lat = js2 %>% filter(Latitude > 45)
+# summary(lat)
+# 
+# 
+# # setwd(dir1)
+# # write.csv(js2, "js.csv", fileEncoding = "CP932")
+
+
+# JS2 ------------------------------------------------------------
+dir_js = "/Users/Yuki/Dropbox/same/JS2"
 setwd(dir_js)
 path = dir_js
 files = list.files(path)
@@ -15,34 +54,17 @@ js = NULL
 for(i in 1:length(files)){
   data = read.csv(paste0(files[i]), fileEncoding = "CP932")
   
-  if("Same" %in% colnames(data)){
-    data2 = data %>% select(CatchDate, Effort_tow, Same, Latitude, Longitude) %>% mutate(sp = "Same") %>% dplyr::rename(kg = Same)
-    kg = data2$kg
-    kg[is.na(kg)] = 0
-    data2$kg = kg
-  }
-  if("SameEi" %in% colnames(data)){
-    data2 = data %>% select(CatchDate, Effort_tow, SameEi, Latitude, Longitude) %>% mutate(sp = "SameEi") %>% dplyr::rename(kg = SameEi)
-    kg = data2$kg
-    kg[is.na(kg)] = 0
-    data2$kg = kg
-  }
+  data2 = data %>% select(CatchDate, Effort_tow, Samerui, Latitude, Longitude) %>% mutate(sp = "Same") %>% dplyr::rename(kg = Samerui)
+  kg = data2$kg
+  kg[is.na(kg)] = 0
+  data2$kg = kg
+  
   js = rbind(js, data2)
 }
-unique(js$CatchDate)
-summary(js$CatchDate)
+summary(js)
+
 js2 = js %>% mutate(year = as.numeric(str_sub(CatchDate, 1, 4)), month = as.numeric(str_sub(CatchDate, 5, 6)), day = as.numeric(str_sub(CatchDate, 7, 8)))
 summary(js2)
-unique(js2$year)
-
-# 緯度経度が間違っている
-lat = js2 %>% filter(Latitude > 45)
-summary(lat)
-
-
-# setwd(dir1)
-# write.csv(js2, "js.csv", fileEncoding = "CP932")
-
 
 
 # PO ------------------------------------------------------------
@@ -79,6 +101,8 @@ unique(po2$year)
 # setwd(dir1)
 # write.csv(po2, "po.csv", fileEncoding = "CP932")
 
+check = po2 %>% filter(Latitude > 44)
+summary(check)
 
 # data area -----------------------------------------------------
 summary(js2)
@@ -86,7 +110,8 @@ summary(po2)
 
 same = rbind(po2, js2) %>% dplyr::rename(lon = Longitude, lat = Latitude)
 summary(same)
-same = same %>% filter(lat < 42) #うまいこと北海道のデータは除去できる
+# 北海道も推定してみる
+# same = same %>% filter(lat < 42) #うまいこと北海道のデータは除去できる
 
 map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
 world_map = map_data("world")
@@ -103,8 +128,9 @@ p = geom_point(data = same , aes(x = lon, y = lat, colour = kg), shape = 16, siz
 # p = geom_point(data = same %>% filter(kg > 10000), aes(x = lon, y = lat, colour = kg), shape = 16, size = 1)
 c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
 labs = labs(x = "Longitude", y = "Latitude", colour = "kg")
+f = facet_wrap(~ year, ncol = 8)
 
-fig = local_map+theme_bw()+th+p+c+labs
+fig = local_map+theme_bw()+th+p+c+labs+f
 ggsave(filename = "map_same.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
 
 
@@ -117,4 +143,3 @@ for(i in 1:12){
   write.csv(data, paste0(file_name, ".csv"), fileEncoding = "CP932")
 }
 
-m1 = same %>% filter(month == 1)
